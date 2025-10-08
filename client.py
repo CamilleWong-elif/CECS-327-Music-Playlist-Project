a98a91bb91f0b54be2251a5a111af1c3bf7e9938
Camille Wong <camyw123@gmail.com>
2025-10-08 10:17:14 -0700

    client class

---
+++ client.py
@@ -0,0 +1,12 @@
+class Client:
+    def __init__(self):
+        self.song = "song name"
+        self.subscription = "fav artist"
+
+    
+
+    def song_request(self):
+        pass
+
+    def receive_notification(self):
+        pass
