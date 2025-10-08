88e5b5e3ddb29c6f7c16fa8b86113442236b4c66
Camille Wong <camyw123@gmail.com>
2025-10-08 10:22:12 -0700

    added main and changed client subscription to list

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 class Client:
     def __init__(self):
         self.song = "song name"
-        self.subscription = "fav artist"
+        self.subscription = [] # list of fav artists client is subscribed to
 
     
 
-    def song_request(self):
-        pass
+    def song_request(self, input_song):
+        return input_song
 
     def receive_notification(self):
         pass
---
+++ main.py
@@ -0,0 +1,18 @@
+from client import Client
+
+def main():
+    play_song = "default song"
+
+
+    # print list of songs
+    print("1. Cruel Summer - Taylor Swift\n" \
+        "2. Box Breathing - Sorry Ghost\n" \
+        "3. Lover - Taylor Swift\n" \
+        "4. Golden - HUNTRX\n" \
+        "5. To The Creatures - Sorry Ghost\n" \
+        "6. Take Down - HUNTRX\n" \
+        "7. How It's Done - HUNTRX\n")
+
+    # get input song from user
+    print("Which song do you want to play?: ", play_song)
+    
